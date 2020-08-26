389bd13fd7b9960d403ba1d42367e07febe8203a
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-08-27 05:53:42 +0900

    Create Discord 봇 온라인 상태 표시하기.py

---
+++ Discord 봇 온라인 상태 표시하기.py
@@ -0,0 +1,12 @@
+#Discord 봇 온라인 상태 표시하기.py
+import discord 
+import asyncio
+client = discord.client
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("Bot Start")
+game = discord.Game("Battle Ground")
+await client.change_presence(status=discord.status.idle, activity=game)
+client.run("봇의 토큰값") #봇의 토큰값 
